7a3ef3faa232a030350ea0eb8a1adc8dfe1bdaa0
Insult <vlad.sipatov@yandex.ru>
2022-10-20 20:53:12 +0300

    add section and js file

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplaySpeed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/solid_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/solid_right.svg"></button>',
+    });
+});
