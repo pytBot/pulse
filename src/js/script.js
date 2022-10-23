648a958f70be8de0b3162fc6703c278b28727806
Insult <vlad.sipatov@yandex.ru>
2022-10-23 16:28:59 +0300

    add section commemts and adaptive carousel

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 
 // Settings
 $(document).ready(function () {
-// Функциона карусели
+    // Функциона карусели
     $('.carousel__inner').slick({
         speed: 1200,
         slidesToShow: 1,
@@ -10,21 +10,33 @@ $(document).ready(function () {
         autoplaySpeed: 1000,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/solid_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/solid_right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                dots: true,
+            }
+
+        }]
     });
-// Функционал табов
+    // Функционал табов
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    $('.catalog-item__link').each(function (i) {
-        $(this).on('click', function (e) {
-            e.preventDefault();
-            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-        })
-    });
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
 
 
